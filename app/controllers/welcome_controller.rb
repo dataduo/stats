5d09f4d7ca6007b0e060d0599260c4ac5bb054e6
Indie <indira@developfor.com>
2013-10-08 23:17:39 +0100

    adding a cdv file not working yet

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,27 @@
 class WelcomeController < ApplicationController
+  require 'csv'
+
   def index
+  	@lines = []
+  	@data_file = File.read('../public/test.csv')
+  	csv_to_array(@data_file, @lines)
+  	# lines[0]["Art"]
   end
 
   def new
   end
+
+
+
+def csv_to_array(file, lines)
+	CSV.foreach(file, headers: true) do |row|
+		lines.push(row.to_hash)
+	end
+end
+
+
+
+
+
+
 end
