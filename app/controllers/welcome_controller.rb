a38d4db9c384e15cede0a8fab5c554c4249f75d7
Indie <indira@developfor.com>
2013-10-09 10:15:38 +0100

    added require csv to application.rb and added a csv folder to the app.  Imported csv in welcome controller and added art score for first person into the welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,24 @@
 class WelcomeController < ApplicationController
-  require 'csv'
+  #require 'csv'
 
   def index
   	@lines = []
-  	@data_file = File.read('../public/test.csv')
-  	csv_to_array(@data_file, @lines)
-  	# lines[0]["Art"]
+  	@data_file = File.read(File.join(Rails.root, 'app','csv','test.csv'))
+  	
+csv = CSV.parse(@data_file , :headers => true)
+csv.each do |row|
+  @lines.push(row.to_hash)
+end
+
+    @art = @lines[0]["Art"]
+
+
+     #csv_to_array(@data_file, @lines)
+
+    # CSV.foreach(@data_file, headers: true) do |row|
+    #   @lines.push(row.to_hash)
+    # end
+  	 #@art = l@ines[0]["Art"]
   end
 
   def new
@@ -13,11 +26,11 @@ class WelcomeController < ApplicationController
 
 
 
-def csv_to_array(file, lines)
-	CSV.foreach(file, headers: true) do |row|
-		lines.push(row.to_hash)
-	end
-end
+# def csv_to_array(@file, @lines)
+# 	CSV.foreach(file, headers: true) do |row|
+# 		@lines.push(row.to_hash)
+# 	end
+# end
 
 
 
